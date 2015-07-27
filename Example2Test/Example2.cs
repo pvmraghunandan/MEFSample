d347545e6ae26df5e4146f5d8d0e43d3df661794
pvmraghunandan <pvmraghunandan.potti@gmail.com>
2015-07-28 00:17:25 +0530

    MEF Sample

---
+++ Example2Test/Example2.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Example1.cs" company="Microsoft"> 
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+//   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+//   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+//   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+//   OTHER DEALINGS IN THE SOFTWARE. 
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Composition;
+
+namespace MEFTest
+{
+    [Export(typeof (IExample))]
+    public class Example2 : IExample, IPartImportsSatisfiedNotification
+    {
+        public void MyAddin()
+        {
+            Console.WriteLine("In Example 2 class");
+        }
+
+        public void OnImportsSatisfied()
+        {
+            Console.WriteLine("Example 2 Imports satisfied");
+        }
+
+        [Export(typeof (Func<int>))]
+        public void sample1(int x)
+        {
+            Console.WriteLine("Sample 1");
+        }
+    }
+
+    //[Export(typeof(IExample))]
+    public class Example3 : IExample
+    {
+        public void MyAddin()
+        {
+            Console.WriteLine("In Example 3 class");
+        }
+
+        [Export(typeof (Func<int>))]
+        public void sample1(int x)
+        {
+            Console.WriteLine("Sample 2");
+        }
+    }
+}
---
+++ MEFTest/MEFSample.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEFTest
+{
+    class MefSample
+    {
+        private CompositionContainer compositionContainer;
+
+        [ImportMany]
+        public IEnumerable<IExample> listofExamples;
+
+        //[ImportMany] //Contract name must match!
+        //public IEnumerable<Func<int>> DoSomething { get; set; }
+
+        public MefSample()
+        {
+            this.Initialize();
+        }
+
+        private void Initialize()
+        {
+            var catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new DirectoryCatalog("Assemblies"));
+            compositionContainer = new CompositionContainer(catalog);
+            try
+            {
+                this.compositionContainer.ComposeParts(this);
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            var mefsample = new MefSample();
+            foreach (var example in mefsample.listofExamples)
+            {
+                example.MyAddin();
+            }
+        }
+    }
+}
